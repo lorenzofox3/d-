b229b96f5862b41763fa18a6fda652435112fad3
lorenzofox3 <laurent34azerty@gmail.com>
2017-09-08 11:38:59 -0400

    upload test

--- scripts/uploadToBucket.js
+++ scripts/uploadToBucket.js
@@ -1,19 +1,25 @@
 const gcs = require('@google-cloud/storage');
 const path = require('path');
-const fs = require('fs');
+const walk = require('walk');
 const storage = gcs({
   projectId: process.env.GCLOUD_PROJECT_ID,
   keyFilename: path.resolve(process.env.HOME, './gcloud-service-key.json')
 });
 
 const bucket = storage.bucket('static.dashgithub.com');
+const distWalker = walk.walk('./dist');
 
-bucket.upload('./dist/index.html',{
-  public:true,
-  dest:'./index.html'
-}, function  (err, res) {
-  if(err){
-    throw err;
-  }
-  console.log(res);
+distWalker.on('file', function (root, fileStats, next) {
+  const rels = root.split('./');
+  const r = rels.slice(2).join('/');
+  const {name} = fileStats;
+  bucket.upload(path.join(root, name), {
+    public: true,
+    dest: path.join(r, name)
+  }, function (err, res) {
+    if (err) {
+      throw err;
+    }
+  });
+  next();
 });
